65f79f1c4b742f3e45b065e070049c9dd43a20bf
beeteam <teambee2015>
2014-01-19 12:35:04 +0100

    test

--- test-java/src/bee/Hello.java
+++ test-java/src/bee/Hello.java
@@ -1,13 +1,13 @@
 package bee;
 
 public class Hello {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
 		System.out.println("guillaume");
 		System.out.println("anthony");
 		System.out.println("anthony2");
+		System.out.println("anthony36");
+		System.out.println("gus");
 	}
-
 }
